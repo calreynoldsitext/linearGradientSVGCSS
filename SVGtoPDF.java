1943f08620e86f66c06a1e433b7ff12917f35713
calreynoldsitext <68069344+calreynoldsitext@users.noreply.github.com>
2020-08-03 14:27:42 -0400

    Add files via upload

---
+++ SVGtoPDF.java
@@ -0,0 +1,39 @@
+package support.JiraArticles;
+
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Image;
+import com.itextpdf.svg.converter.SvgConverter;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class SVGtoPDF {
+
+    public static final String DEST = "output_file_path/SVGOutput.pdf";
+    public static final String SVG = "input_file_path/linearGradient.svg";
+
+    public static void main(String[] args) throws IOException {
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+        new SVGtoPDF().createPdf();
+    }
+
+    public void createPdf() throws IOException {
+        try (FileInputStream svg = new FileInputStream(SVG))
+        {
+
+            // This approach (commented below) directly converts from SVG to PDF.
+            //SvgConverter.createPdf(new File(SVG), new File(DEST));
+
+            // This approach (below) creates a PDF document, then adds SVG to that document.
+            PdfDocument pdf = new PdfDocument(new PdfWriter(DEST));
+            Document doc = new Document(pdf);
+            Image converted = SvgConverter.convertToImage(svg, pdf);
+            doc.add(converted);
+            doc.close();
+        }
+    }
+}
---
+++ linearGradientSVG.cs
@@ -0,0 +1,37 @@
+using System;
+using System.IO;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using iText.Svg.Converter;
+
+namespace Example
+{
+    public class SVGtoPDF
+    {
+        public const String DEST = "output_file_path/SVGOutput.pdf";
+        public const String SVG = "input_file_path/linearGradient.svg";
+        public static void Main(String[] args)
+        {
+            FileInfo file = new FileInfo(DEST);
+            file.Directory.Create();
+            new SVGtoPDF().CreatePdf(DEST);
+        }
+
+        public virtual void CreatePdf(String dest)
+        {
+            using (FileStream svg = File.Open(SVG, FileMode.Open))
+            {
+                // This approach (commented below) directly converts from SVG to PDF.
+                //SvgConverter.CreatePdf(new FileInfo(SVG), new FileInfo(DEST));
+
+                // This approach (below) creates a PDF document, then adds SVG to that document.
+                PdfDocument pdf = new PdfDocument(new PdfWriter(DEST));
+                Document doc = new Document(pdf);
+                Image converted = SvgConverter.ConvertToImage(svg, pdf);
+                doc.Add(converted);
+                doc.Close();
+            }
+        }
+    }
+}
